f8a397451801a40d3eab513edfc43a8b78aa5024
Erick Castillo <castilloerick00@gmail.com>
2023-06-22 23:33:33 -0500

    Comentario archivo "teclas.cpp"
    En este cambio se realizo el comentario detallado del archivo teclas.cpp, correspondiente al archivo con las funciones anti rebote de los pulsantes.

--- proyecto_semaforo/src/teclas.cpp
+++ proyecto_semaforo/src/teclas.cpp
@@ -1,67 +1,72 @@
+// Importamos el header del programa teclas
 #include "teclas.h"
 
-// Comentario de prueba 
-
+// Función para actualizar el estado de los pulsantes
 void pulsantes(int sw){
-  switch (estadoActual) {
+  switch (estadoActual) {               // Posibles estados de los pulsantes
+    // Este caso corresponde al estado donde no se a presionado el pulsante
     case Inactivo:
-      if (digitalRead(sw) == LOW) {
-        estadoActual = Antirrebote;
-        tiempoUltimoCambio = millis();
+      if (digitalRead(sw) == LOW) {     // Si se detecta el cambio de estado (PRESIONAMOS)
+        estadoActual = Antirrebote;     // Actualización del estado
+        tiempoUltimoCambio = millis();  // Tiempo de ejecución del programa
       }
       break;
-      
+    
+    // Este caso corresponde al estado donde se mantiene presionado el pulsante
     case Activo:
-      if (digitalRead(sw) == HIGH) {
-        estadoActual = Antirrebote;
-        tiempoUltimoCambio = millis();
+      if (digitalRead(sw) == HIGH) {    // Si se detecta el cambio de estado (SOLTAMOS)
+        estadoActual = Antirrebote;     // Actualización del estado
+        tiempoUltimoCambio = millis();  // Tiempo de ejecución del programa
       }
       break;
-      
+    
+    // Este caso corresponde al estado donde se presiona o se suelta el pulsante (ANTIRREBOTE)
     case Antirrebote:
-      if (millis() - tiempoUltimoCambio >= tiempoRebote) {
-        if (digitalRead(sw) == LOW) {
-          estadoActual = Activo;
-        } else {
-          estadoActual = Inactivo;
+      if (millis() - tiempoUltimoCambio >= tiempoRebote) {  // Transcurrido un tiempo determinado
+        if (digitalRead(sw) == LOW) {                       // Si se presiono el pulsante 
+          estadoActual = Activo;                            // Actualización del estado
+        } else {                                            // Si se solto el pulsante
+          estadoActual = Inactivo;                          // Actualización del estado
         }
       }
       break;
   }
 }
 
+// Función para la lectura del pulsante del cambio de MODO
 void condicion_antirrebote(int pul) {
-  pulsantes(pul);
-  if (estadoActual == Antirrebote){
-    while(1){
+  pulsantes(pul);                         // Detectamos un pulso
+  if (estadoActual == Antirrebote){   
+    while(1){                             // Esperamos que se finalice el antirrebote
       pulsantes(pul);
-      if (estadoActual == Activo){
+      if (estadoActual == Activo){    
         break;
       }
     }
-    if (estadoActual == Activo){
-      while(1){
+    if (estadoActual == Activo){          // Si se encuentra presionado
+      while(1){                           // Esperamos que se suelte el pulsante
         pulsantes(pul);
-        if (estadoActual == Antirrebote){
+        if (estadoActual == Antirrebote){ // Si se suelta el pulsante, rompemos el bucle
           break;
         }
       }
     }
-    if (estadoActual == Antirrebote){
-      while(1){
-        pulsantes(pul);
-        if (estadoActual == Inactivo){
+    if (estadoActual == Antirrebote){    
+      while(1){                           // Esperamos que finalice el antirrebote
+        pulsantes(pul); 
+        if (estadoActual == Inactivo){    // Verificamos si se solto el pulsante
           break;
         }
       }
 
-      if (modo == NORMAL){
+      // En el caso de haber detectado un pulso
+      if (modo == NORMAL){              // Cambiamos de modo NORMAL a modo DESCONECTADO
         modo = DESCONECTADO;
         estado_actual = D_AMARILLO;}
-      else if (modo == DESCONECTADO){
-        modo = ALARMA;
+      else if (modo == DESCONECTADO){   // Cambiamos de modo DESCONECTADO a modo ALARMA
+        modo = ALARMA;  
         estado_actual = ESTADO_ROJO;}
-      else {
+      else {                            // Cambiamos de modo ALARMA a modo NORMAL
         modo = NORMAL;
         estado_actual = ESTADO_VERDE;
       }
@@ -69,52 +74,49 @@ void condicion_antirrebote(int pul) {
   }
 }
 
-
+// Función para la lectura del pulsante del cambio de VELOCIDAD
 void condicion_anti(int pulsa) {
-  pulsantes(pulsa);
-  if (estadoActual == Antirrebote){
-    while(1){
+  pulsantes(pulsa);                       // Detectamos un pulso
+  if (estadoActual == Antirrebote){ 
+    while(1){                             // Esperamos que se finalice el antirrebote
       pulsantes(pulsa);
-      if (estadoActual == Activo){
+      if (estadoActual == Activo){        
         break;
       }
     }
-    if (estadoActual == Activo){
-      while(1){
+    if (estadoActual == Activo){          // Si se encuentra presionado
+      while(1){                           // Esperamos que se suelte el pulsante
         pulsantes(pulsa);
-        if (estadoActual == Antirrebote){
+        if (estadoActual == Antirrebote){ // Si se suelta el pulsante, rompemos el bucle
           break;
         }
       }
     }
     if (estadoActual == Antirrebote){
-      while(1){
+      while(1){                           // Esperamos que finalice el antirrebote
         pulsantes(pulsa);
-        if (estadoActual == Inactivo){
+        if (estadoActual == Inactivo){    // Verificamos si se solto el pulsante
           break;
         }
       }
-
-      // return true;
-
-       if (velocidad == 1){
-              t1 = 500;
-              t2 = 3000;
-              t3 = 500;
-              velocidad = 2;}
-            else if (velocidad == 2){
-              t1 = 500;
-              t2 = 500;
-              t3 = 3000;
-              velocidad = 3;}
-            else {
-              t1 = 3000;
-              t2 = 500;
-              t3 = 500;
-              velocidad = 1;
-          }
- 
-
+      
+      // En el caso de haber detectado un pulso
+      if (velocidad == 1){          // Actualizamos la velocidad (x2)
+        t1 = 1500;
+        t2 = 250;
+        t3 = 500;
+        velocidad = 2;}
+      else if (velocidad == 2){     // Actualizamos la velocidad (x0.5)
+        t1 = 6000;
+        t2 = 1000;
+        t3 = 2000;
+        velocidad = 3;}             // Actualizamos la velocidad (x1)
+      else {
+        t1 = 3000;
+        t2 = 500;
+        t3 = 1000;
+        velocidad = 1;
       }
-      }
+    }
+  }
 }

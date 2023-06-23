7d86ccb2e76956ba3eb6cfd8c5dffa84fe3e4986
Erick Castillo <castilloerick00@gmail.com>
2023-06-22 22:45:16 -0500

    Comentario archivo "uart.h"
    En este cambio se realizo el comentario detallado del archivo uart.h, correspondiente al header del archivo principal de la practica del semáforo.

--- proyecto_semaforo/src/uart.h
+++ proyecto_semaforo/src/uart.h
@@ -1,8 +1,8 @@
+// Generamos la estructura principal del archivo header para uart.cpp
 #ifndef UART_H
 #define UART_H
 
+// Llamamos el archivo semaforo.h, para importar las funciones del semáforo
 #include "semaforo.h"
-
-
 
 #endif 

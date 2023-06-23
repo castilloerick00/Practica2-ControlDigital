cf7c0a3caab3975c255ccf275ac42b3313aeb8b7
Erick Castillo <castilloerick00@gmail.com>
2023-06-22 23:38:29 -0500

    Comentario archivo "teclas.h"
    En este cambio se realizo el comentario detallado del archivo teclas.h, correspondiente al header del archivo con las funciones anti rebote de los pulsantes.

--- proyecto_semaforo/src/teclas.h
+++ proyecto_semaforo/src/teclas.h
@@ -1,6 +1,8 @@
+// Generamos la estructura principal del archivo header para teclas.cpp
 #ifndef TECLAS_H
 #define TECLAS_H
 
+// Llamamos a la libreria de arduino
 #include <Arduino.h>
 
 // Definición de los estados del semáforo
@@ -10,39 +12,40 @@ const int ESTADO_VERDE = 2;
 const int D_AMARILLO = 3;
 const int D_APAGADO = 4;
 
+// Definición de los modos del semáforo
 const int NORMAL = 0;
 const int DESCONECTADO = 1;
 const int ALARMA = 2;
 
+// Exportamos los tiempos 
 extern int t1;
 extern int t2;
 extern int t3;
 
+// Exportamos la bandera de velocidad
 extern int velocidad;
 
-// Variable para almacenar el estado actual del semáforo
+// Exportamos la variable para almacenar el estado actual del semáforo
 extern int estado_actual;
 
-// Varaiable para almacenar el estado actual del modo de operacion 
+// Exportamos la varaiable para almacenar el estado actual del modo de operacion 
 extern int modo;
 
-
-
-// ANTIRREBORTE
+// Definimos un tiempo para el ANTIRREBORTE
 const unsigned long tiempoRebote = 100; // Tiempo en milisegundos
 
-// Estados de la máquina de estados
+// Estados de la máquina de estados para los pulsantes
 enum Estados {
   Inactivo,
   Activo,
   Antirrebote
 };
 
-// Variables globales
+// Exportamos el estado actual de los pulsantes y el tiempo del ultimo cambio 
 extern Estados estadoActual;
 extern int tiempoUltimoCambio;
 
-
+// Inicializamos las funciones programadas en semaforo.cpp, para luego llamar en el programa principal
 void pulsantes(int sw);
 void condicion_antirrebote(int pul);
 void condicion_anti(int pulsa);

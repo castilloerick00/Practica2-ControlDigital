cad2b9c685260d09631fc4323ed4c36f57de997f
Erick Castillo <castilloerick00@gmail.com>
2023-06-22 23:11:39 -0500

    Comentario archivo "semaforo.h"
    En este cambio se realizo el comentario detallado del archivo semaforo.h, correspondiente al header del archivo con las funciones de actualización del modo de operación del semáforo.

--- proyecto_semaforo/src/semaforo.h
+++ proyecto_semaforo/src/semaforo.h
@@ -1,14 +1,14 @@
+// Generamos la estructura principal del archivo header para semaforo.cpp
 #ifndef SEMAFORO_H
 #define SEMAFORO_H
 
+// Llamamos el archivo teclas.h, para importar las funciones de los pulsantes
 #include "teclas.h"
 
-
-
+// Inicializamos las funciones programadas en semaforo.cpp, para luego llamar en el programa principal
 void inicializar_semaforo();
 void Espera(int valor);
 void actualizar_semaforo();
 void actualizar_modo();
-
 
 #endif
